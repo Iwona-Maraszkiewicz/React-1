e776a4dc4987590cb236e73040624e02d2674f5e
Iwona Maraszkiewicz <ikonopinska@gmail.com>
2021-06-10 15:34:01 +0200

    Add creator column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
+import Card from '../Card/Card.js';
 
 class Column extends React.Component {
+  state = {
+    cards: this.props.cards || [],
+  }
   static propTypes = {
     Column: PropTypes.string,
   }
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.column}</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
       </section>
     )
   }
