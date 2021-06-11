3b732196c59513a2603e166ac9adc3cda28dc9de
Iwona Maraszkiewicz <ikonopinska@gmail.com>
2021-06-11 14:22:35 +0200

    Add Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,11 +6,12 @@ class Card extends React.Component {
   static propTypes = {
     title: PropTypes.node,
   }
+
   render() {
     const {title} = this.props;
     return (
       <section className={styles.component}>
-        <h3>{title}</h3>
+        <h3>{this.props.title}</h3>
       </section>
     )
   }
