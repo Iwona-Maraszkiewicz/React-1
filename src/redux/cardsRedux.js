ce7843418df633363999bf17a234e25007a532b4
Iwona Maraszkiewicz <ikonopinska@gmail.com>
2021-06-18 18:42:54 +0200

    Add search

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,7 +1,7 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+export const getCardsForColumn = ({cards, searchString}, columnId) => cards.filter((card) => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title));
 
 // action name creator
 const reducerName = 'cards';
@@ -14,11 +14,11 @@ export const ADD_CARD = createActionName('ADD_CARD');
 export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
 
 // reducer
-export default function reducer(statePart = [], action = {}) {
+export default function reducer(state = [], action = {}) {
   switch (action.type) {
     case ADD_CARD:
-      return [...statePart, action.payload];
+      return [...state, action.payload];
     default:
-      return statePart;
+      return state;
   }
 }
