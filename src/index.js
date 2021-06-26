fd1598c64bcaae77d3f6fc2c6bbfb1f397d3dd2e
Iwona Maraszkiewicz <ikonopinska@gmail.com>
2021-06-26 16:20:03 +0200

    Add ListLInk Search

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App.js';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
